4e46f5e92768f22ad84eab0e472dc912e75f55f3
Kilzera02 <kil.teixeira@sou.inteli.edu.br>
2022-05-26 20:10:20 -0300

    Atualização do currículo com BD

--- 03_AUT_EST_ENTREGA/Semana 6/backend/app_61.js
+++ 03_AUT_EST_ENTREGA/Semana 6/backend/app_61.js
@@ -4,13 +4,12 @@ const app = express();
 
 const hostname = '127.0.0.1';
 const port = 3061;
+app.use(express.static("../frontend/"));
 
 const sqlite3 = require('sqlite3').verbose(); 
 const DBPATH = 'dbUser.db'; 
 const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
-
-
 /* Definição dos endpoints */
 
 /****** CRUD ******************************************************************/
